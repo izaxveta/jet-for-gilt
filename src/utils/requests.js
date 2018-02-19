1598555f22f0be4ef0777af81cdec9aae93a296c
izaxveta <izavetha@gmail.com>
2018-02-18 19:03:13 -0700

    Build handle response function for all fetch calls

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,4 +1,19 @@
 
+const handleResponse = (response) => {
+  return response.json()
+    .then((json) => {
+      if (!response.ok) {
+        const error = {
+          status: response.status,
+          statusText: response.statusText,
+          json
+        }
+        return Promise.reject(error)
+      }
+      return json
+    })
+}
+
 module.exports = {
   addUser
 }
