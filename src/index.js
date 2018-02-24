9d276fff95ab16f2712573e53f9ffa8af6383096
izaxveta <izavetha@gmail.com>
2018-02-24 13:13:47 -0700

    Initialize react-router implementation to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
-import WelcomeForm from './components/WelcomeForm'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<WelcomeForm />, document.getElementById('landing-container'))
+import  Landing  from './components/Landing'
+import  Dashboard  from './components/Dashboard'
+
+import './styles/index.css'
+// ReactDOM.render(<App />,  document.getElementById('landing-container'))
+
+ReactDOM.render(
+  <Router>
+    <div class='app'>
+      <Route exact path='/' component={ Landing } />
+      <Route exact path='/dashboard' component={ Dashboard } />
+    </div>
+  </Router>,
+  document.getElementById('root')
+)
 
 registerServiceWorker()
