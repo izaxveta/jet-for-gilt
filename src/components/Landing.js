5dc4b841d0aa35a051803205115bfff3c1891820
izaxveta <izavetha@gmail.com>
2018-02-26 02:37:17 -0700

    Modify Landing component to render different values depending on the state mode value.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,81 +2,54 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import SignInForm from './SignInForm'
 import RegisterForm from './RegisterForm'
+import Video from './Video'
 import '../styles/Landing.css'
 
 class Landing extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      firstName: '',
-      lastName:  '',
-      email:     '',
-      password:  ''
+      mode: 'register'
     }
   }
 
-  updateUser = (key, event) => {
-    this.setState({ [key]: event.target.value })
+  onClickSignIn = (event) => {
+    event.preventDefault()
+    this.setState({ mode: 'sign-in' })
   }
 
-  renderSignInForm = (event) => {
-    let welcomeContainer = document.getElementsByClassName('form-container welcome-container')[0]
-    welcomeContainer.innerHTML = ''
-    ReactDOM.render(<SignInForm />, welcomeContainer)
+  componentDidMount() {
+    let videoContainer = document.getElementById('video')
+    ReactDOM.render(<Video />, videoContainer)
   }
-
+  renderWelcomeContainer = () => {
+    if (this.state.mode === 'register') {
+      return (
+        <React.Fragment>
+          <RegisterForm />
+          <div className='line-divider'></div>
+          <div className='sign-in'>
+            <p>Have an account? Sign in using the link below.</p>
+            <button type='submit' className='signin-button' onClick={ this.onClickSignIn }>SIGN IN</button>
+          </div>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <React.Fragment>
+          <SignInForm />
+        </React.Fragment>
+      )
+    }
+  }
 
   render() {
     return (
       <div className='form-container welcome-container'>
-        <RegisterForm />
-        <div className='line-divider'></div>
-        <div className='sign-in'>
-          <p>Have an account? Sign in using the link below.</p>
-          <button type='submit' className='signin-button' onClick={ this.renderSignInForm }>SIGN IN</button>
-        </div>
+        { this.renderWelcomeContainer() }
       </div>
     )
   }
 }
 
 export default Landing
-// class App extends Component {
-
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       firstName: '',
-//       lastName:  '',
-//       email:     '',
-//       password:  ''
-//     }
-//   }
-
-//   updateUser = (key, event) => {
-//     this.setState({ [key]: event.target.value })
-//   }
-
-//   renderSignInForm = (event) => {
-//     let welcomeContainer = document.getElementsByClassName('form-container welcome-container')[0]
-//     welcomeContainer.innerHTML = ''
-//     ReactDOM.render(<SignInForm />, welcomeContainer)
-//   }
-
-//   render() {
-//     return (
-//       <div className='container'>
-//         <div className='form-container welcome-container'>
-//           <RegisterForm />
-//           <div className='line-divider'></div>
-//           <div className='sign-in'>
-//             <p>Have an account? Sign in using the link below.</p>
-//             <button type='submit' className='signin-button' onClick={ this.renderSignInForm }>SIGN IN</button>
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App
