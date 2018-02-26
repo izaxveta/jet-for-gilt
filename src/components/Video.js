bbc1b78b16ab77bfcdc44a1df562ac28d755cb02
izaxveta <izavetha@gmail.com>
2018-02-26 02:35:09 -0700

    Pull video out of html and into separate object to be rendered when Landing component mounts and on no other pages.

---
+++ src/components/Video.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Landing.css'
+
+const Video = () => {
+  return (
+    <iframe frameborder="0" width="100%" height="100%" src="https://www.youtube.com/embed/Giu7iWh1rd4?rel=0&controls=0&showinfo=0&autoplay=1&autohide=1&mute=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen volume="0"></iframe>
+  )
+}
+
+export default Video
