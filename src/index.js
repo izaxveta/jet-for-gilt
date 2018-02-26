8e11c927617fa2db1708981b572c404d06fa3dd6
izaxveta <izavetha@gmail.com>
2018-02-25 17:33:40 -0700

    Implement BrowserRouter to render App component into root element

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
-
-import  Landing  from './components/Landing'
-import  Dashboard  from './components/Dashboard'
-
+import App from './components/App'
 import './styles/index.css'
 // ReactDOM.render(<App />,  document.getElementById('landing-container'))
 
 ReactDOM.render(
-  <Router>
-    <div class='app'>
-      <Route exact path='/' component={ Landing } />
-      <Route exact path='/dashboard' component={ Dashboard } />
-    </div>
-  </Router>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+  </BrowserRouter>,
   document.getElementById('root')
 )
 
