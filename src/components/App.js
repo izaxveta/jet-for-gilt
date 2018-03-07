89efda568aa9e51f2b21ef79dee6d3046ce3e6f7
izaxveta <izavetha@gmail.com>
2018-03-07 15:35:33 -0700

    Open /dashboard/:moniker/edit route for uses editing their information

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends Component {
       <div className="app">
         <Switch>
           <Route exact path='/' component={ Landing }/>
+          <Route path='/dashboard/:moniker/edit' component = { UserEdit }/>
           <Route path='/dashboard/:moniker' component = { Dashboard }/>
         </Switch>
       </div>
