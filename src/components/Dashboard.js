3c60f6517848fce6aae4acb356cfc42ad0e1f0d6
izaxveta <izavetha@gmail.com>
2018-03-06 21:13:36 -0700

    Call setCurrentUser function to define user in localStorage immediately after signin

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,6 +14,7 @@ class Dashboard extends Component {
 
   componentWillMount() {
     document.getElementById('video-background').innerHTML = ''
+    setCurrentUser(this.state.moniker, this.props.location.state.password)
   }
 
   render() {
