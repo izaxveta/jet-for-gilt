0af4c8f296b7dd519394b2f1077496fd49acd65f
izaxveta <izavetha@gmail.com>
2018-03-07 16:08:41 -0700

    Initialize UserEdit component

--- src/components/UserEdit.js
+++ src/components/UserEdit.js
@@ -1,2 +1,12 @@
+import React, { Component } from 'react'
 import NavBar from './NavBar'
+
+class UserEdit extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
         <NavBar />
+}
+
