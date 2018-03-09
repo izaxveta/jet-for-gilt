c20ef7ac7fba8732b7d8c57ef718cc682b9e618a
izaxveta <izavetha@gmail.com>
2018-03-09 00:01:27 -0700

    Remove commented out code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import registerServiceWorker from './registerServiceWorker'
 import App from './components/App'
 import NavBar from './components/NavBar'
 import './styles/index.css'
-// ReactDOM.render(<App />,  document.getElementById('landing-container'))
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
