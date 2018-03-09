6b9c09f0fab01e5f2cd8fe073d79cd7b756e8898
izaxveta <izavetha@gmail.com>
2018-03-09 00:19:55 -0700

    Initialize Venue component

---
+++ src/components/Venue.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import VenueProfile from './VenueProfile'
+import '../styles/Venue.css'
+
+class Venue extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+}
+
+export default Venue
