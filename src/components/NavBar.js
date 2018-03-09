0a5c013d7b9c0a30990db3677a72686487eb6ab5
izaxveta <izavetha@gmail.com>
2018-03-09 01:36:12 -0700

    Modify currentUser stated to call checkUser function - currentUser will be defined when user is available in localStorage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentUser: JSON.parse(localStorage.getItem('user')).user
+      currentUser: this.checkUser()
     }
   }
 
