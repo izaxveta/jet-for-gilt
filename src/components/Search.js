ab6854905954acd0512198bfdcf9c3bd5a0c7a70
izaxveta <izavetha@gmail.com>
2018-03-09 01:00:34 -0700

    Disable NavBar component while testing NavBar functionality in index.js file

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,6 +27,7 @@ class Search extends Component {
   render = () => {
     return (
       <React.Fragment>
+        {/* <NavBar /> */}
         <h3>{ this.state.venues.length } Results</h3>
         <VenueCollection venues={ this.state.venues } />
       </React.Fragment>
