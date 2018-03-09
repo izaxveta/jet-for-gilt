d2911766bc5b7ccf3fc4d044b1e8ddba7abb2865
izaxveta <izavetha@gmail.com>
2018-03-09 00:20:27 -0700

    Initialize VenueCard prop

---
+++ src/components/VenueCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../styles/VenueCard.css'
+
+const VenueCard = (props) => {
+  const venue = props.venue
+}
+
+export default VenueCard
