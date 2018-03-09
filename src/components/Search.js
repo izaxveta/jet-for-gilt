69efe3d4cf6ab00afc31ae66c03c24d1bcbaf20f
izaxveta <izavetha@gmail.com>
2018-03-09 00:20:45 -0700

    Initialize Search component

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { getAllVenues } from '../utils/requests'
+import VenueCollection from './VenueCollection'
+import NavBar from './NavBar'
+import '../styles/Search.css'
+
+class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+}
+
+export default Search
--- src/components/VenueCard.js
+++ src/components/VenueCard.js
@@ -5,6 +5,42 @@ import '../styles/VenueCard.css'
 
 const VenueCard = (props) => {
   const venue = props.venue
+  return (
+    <div className='venue-container'>
+      <Link to={{ pathname: `/venue/${venue.name}`, state: { venue: venue }}}>
+        <div className='venue-listing'>
+          <img src={ venue.image_url } alt='' className='listing-venue-image'/>
+          <div className='venue-listing-info'>
+            <div className='grouped-info-a'>
+              <div className='venue-name-category'>
+                <div className='listing-name'>{ venue.name }</div>
+                <div className='listing-categories'>{ displayVenueCategories(venue.categories) }</div>
+              </div>
+              <div className='listing-location'>{ displayVenueLocation(venue.location) }</div>
+            </div>
+            <div className='grouped-info-b'>
+              <div className='venue-price-rating'>
+                <div className='listing-price'>{ venue.price }</div>
+                <div className='popularity'>
+                  <div className='listing-rating'>{ venue.rating }/5</div>
+                  <div className='listing-review_count'>({ venue.review_count } Ratings)</div>
+                </div>
+              </div>
+              <div className='listing-phone'>{ venue.phone }</div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+const displayVenueCategories = (categoryCollection) => {
+  return categoryCollection.join(', ')
+}
+
+const displayVenueLocation = (location) => {
+  return location.join('\n')
 }
 
 export default VenueCard
