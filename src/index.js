9be6b1bdfd98feb2cd3960f39a29321f590d4812
izaxveta <izavetha@gmail.com>
2018-03-09 00:01:19 -0700

    Import NavBar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 import App from './components/App'
+import NavBar from './components/NavBar'
 import './styles/index.css'
 // ReactDOM.render(<App />,  document.getElementById('landing-container'))
 
