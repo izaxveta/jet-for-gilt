0adbf451f0f267c4ee8de8bfe81298a70a9bcbe8
izaxveta <izavetha@gmail.com>
2018-03-09 00:29:52 -0700

    Initialize VenueCollection component

---
+++ src/components/VenueCollection.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import VenueCard from './VenueCard'
+
+class VenueCollection extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+}
+
+export default VenueCollection
