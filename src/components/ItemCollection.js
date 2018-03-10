76035e56d5b4a69f3b5f7f1cd6aee8ab56ab40db
izaxveta <izavetha@gmail.com>
2018-03-09 21:48:44 -0700

    ItemCollection render function to call renderItemCards function - creates cards for each item in collection

--- src/components/ItemCollection.js
+++ src/components/ItemCollection.js
@@ -5,6 +5,19 @@ import ItemCard from './ItemCard'
 class ItemCollection extends Component {
   constructor(props) {
     super(props)
+
+  renderItemCards = () => {
+    this.state.items.map((item) =>
+      (<ItemCard item={ item } />)
+    )
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        { this.renderItemCards() }
+      </React.Fragment>
+    )
   }
 }
 
