7d3969af810f30efdc8a30b5f7e623617483eda6
izaxveta <izavetha@gmail.com>
2018-03-09 21:47:59 -0700

    Initialize ItemCollection component

---
+++ src/components/ItemCollection.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import ItemCard from './ItemCard'
+
+class ItemCollection extends Component {
+  constructor(props) {
+    super(props)
+  }
+}
+
+export default ItemCollection
